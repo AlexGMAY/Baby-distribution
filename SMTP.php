c06dcdeeef8d98ea774b280285296a0cbb304edc
Merveille Alexander <64960547+AlexGMAY@users.noreply.github.com>
2021-01-06 11:07:30 +0100

    Add files via upload

---
+++ SMTP.php
@@ -0,0 +1,10 @@
+<?php
+define("host", "mail.baby-distribution.com");
+define("SMTPAuth", "true");
+define("username","commercial@baby-distribution.com");
+define("password","NS,eY1IEgakG");
+define("SMTPSecure","ssl");
+define("Port","465");
+
+
+?>
